1443de30c2efefd54535930e2dcc140c5bc9e7ce
khatanboyevelbek <programmerelbek@gmail.com>
2023-02-13 22:26:28 +0500

    BROKERS: Employee Model & Migration

---
+++ SynelTask.Web/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,6 @@
+﻿namespace SynelTask.Web.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+    }
+}
---
+++ SynelTask.Web/Brokers/Storages/StorageBroker.Employee.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SynelTask.Web.Models.Foundations.Employees;
+
+namespace SynelTask.Web.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
